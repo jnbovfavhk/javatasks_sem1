46ab0e66103a0954c8bcbb7b9f1b81197e34d7da
д а <ilya.belonojko@gmail.com>
2024-10-23 21:49:30 +0400

    did 3rd, 4th and 5th tasks

--- src/main/java/org/knit/jnbovfavhk/Main.java
+++ src/main/java/org/knit/jnbovfavhk/Main.java
@@ -1,9 +1,16 @@
 package org.knit.jnbovfavhk;
 
+import org.knit.jnbovfavhk.lab2.FileSystemComponent;
+
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        int[] a = new int[10];
+        a[0] = 1;
+        System.out.println(Arrays.stream(a).filter(x -> x != 0).toArray().length);
         org.knit.jnbovfavhk.lab2.Main.main();
     }
 }
