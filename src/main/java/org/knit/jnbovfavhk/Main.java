e5df196b22f1e8ff9f21af2d0df87fda562259c0
д а <ilya.belonojko@gmail.com>
2024-12-14 00:12:16 +0400

    did 10th task

--- src/main/java/org/knit/jnbovfavhk/Main.java
+++ src/main/java/org/knit/jnbovfavhk/Main.java
@@ -6,7 +6,8 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IllegalAccessException {
 //        org.knit.jnbovfavhk.lab2.Main.main();
-        org.knit.jnbovfavhk.lab5.Main.main();
-        org.knit.jnbovfavhk.lab9.Main.main();
+//        org.knit.jnbovfavhk.lab5.Main.main();
+//        org.knit.jnbovfavhk.lab9.Main.main();
+        org.knit.jnbovfavhk.lab6.Main.main();
     }
 }
---
+++ src/main/java/org/knit/jnbovfavhk/lab6/Main.java
@@ -0,0 +1,9 @@
+package org.knit.jnbovfavhk.lab6;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main() throws FileNotFoundException {
+        Task10.execute();
+    }
+}
